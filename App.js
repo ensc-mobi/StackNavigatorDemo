a5a27ae3d6957a7b65f53885cde08dff60a7026a
Baptiste Pesquet <bpesquet@gmail.com>
2023-01-11 13:50:50 +0100

    Add details screen, navigation and common header

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Ecran d'accueil</Text>
+      <Text style={styles.text}>This is the home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          // Navigate to the Details route with 2 params
+          navigation.navigate("Details", {
+            itemId: 86,
+            otherParam: "anything you want here",
+          });
+        }}
+      />
+    </View>
+  );
+};
+
+const DetailsScreen = ({ navigation, route }) => {
+  // Get the params
+  const { itemId, otherParam } = route.params;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the details screen</Text>
+      {/* Convert params to JSON strings before display */}
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => {
+          // Navigate to Details again with one param
+          navigation.push("Details", {
+            itemId: Math.floor(Math.random() * 100),
+          });
+        }}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
     </View>
   );
 };
@@ -16,8 +50,25 @@ const Stack = createNativeStackNavigator();
 export default App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Accueil" component={HomeScreen} />
+      <StatusBar barStyle="light-content" backgroundColor="#f4511e" />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "My home" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -25,4 +76,5 @@ export default App = () => {
 
 const styles = StyleSheet.create({
   container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  text: { fontSize: 18, paddingBottom: 10 },
 });
